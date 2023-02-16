008f76cdba5d99bc50b5a2c55b1fe282f7758609
14403 <a7la_soko@yahoo.com>
2023-02-16 09:33:15 -0500

    Action

--- src/test/java/technostudyB7/day7/GetCssValue.java
+++ src/test/java/technostudyB7/day7/GetCssValue.java
@@ -1,17 +1,29 @@
 package technostudyB7.day7;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetCssValue {
+public class GetCssValue extends UtilityClass{
     public static void main(String[] args) {
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-
         driver.get("https://www.snapdeal.com");
+        WebElement searchInput=driver.findElement(By.id("inputValEnter"));
+        searchInput.getAttribute("autocomplete");
+        System.out.println(searchInput.getAttribute("autocomplete"));
+        System.out.println(   searchInput.getAttribute("name"));
+
+searchInput.getAttribute("id");
+        System.out.println(searchInput.getAttribute("id"));
+        searchInput.getCssValue("background");
+
+        searchInput.getCssValue("font family");
+        System.out.println(searchInput.getCssValue("font-family"));
+
+        searchInput.getCssValue(":color");
+        System.out.println(searchInput.getCssValue("color"));
 
 
     }
